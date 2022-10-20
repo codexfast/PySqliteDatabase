9a57496c23a6122c7078c4ab2aa9e705ce776658
Gilberto <codexfast@gmail.com>
2022-10-20 17:38:26 -0300

    some changes

--- PyDataSqlite/database.py
+++ PyDataSqlite/database.py
@@ -12,14 +12,37 @@ class Database:
     """
         Class that handles database
         
-        The 'name' variable can also contain the exact database path
+        The 'pathname' variable can also contain the exact database path
     """
-    name : str = field(default=':memory:')
-    _conn : sqlite3.Connection = field(repr=False, init=False)
+    pathname : str = field(default=':memory:')
+    _conn : sqlite3.Connection = field(default=":memory:", repr=False)
 
-    def __pos_init__(self):
-        self._conn = sqlite3.connect(self.name)
+    def __post_init__(self):
+        try:
+            self._conn = sqlite3.connect(self.pathname)
+        except sqlite3.Error as err:
+            print(err, f'({self.pathname})')
+
+    def __del__(self):
+        if isinstance(self._conn, sqlite3.Connection):
+            self._conn.close()
+
+    def run(self, sql : str):
+        """
+            Here execute sql statement
+        """
+        return None
+
+    @staticmethod
+    def create_conn(pathname : str):
+        """
+            Static method for quick connection creation
+        """
+        return sqlite3.connect(pathname)
 
 if __name__ == "__main__":
-    db = Database('test.sqlite')
-    print(db)
+    # db = Database('test.sqlite')
+    db = Database()
+    print(db._conn)
+    
+    # input("trava >>")
